85ec86435066e12bbe8c74b9cadc4d3612ede512
azize <azizetrousse1@gmail.com>
2020-06-19 04:59:35 -0700

    ok

--- templates/fouter.php
+++ templates/fouter.php
@@ -2,11 +2,5 @@
 <div class="map">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13627.576639744166!2d-8.525047319254003!3d31.36189977548003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdae2a91f10682e7%3A0xa2d58bb2bae72705!2sMejjat!5e0!3m2!1sfr!2sma!4v1592140516576!5m2!1sfr!2sma" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </div>
-<div class="contact" >
-    <ul>
-        <a href="">home</a>
-        <a href="">home</a>
-        <a href="">home</a>
-</ul>
-</div>
+
 </footer>
--- templates/home.php
+++ templates/home.php
@@ -5,14 +5,19 @@ include 'data/db.php';
 <article>
    <?php for($i=0;$i<count($data['pages']); $i++){ ?>
     <div class="box">
-    <a href="templates/post.php?p=<?= $i?>">
+    
        <div class="informtion">
          <h4><?= $data ['pages'][$i] ['title'] ?></h4>
         </div>
         <div class="img">
              <img src="./image/<?= $data ['pages'][$i] ['img'] ?>" alt="">
+             <a href="templates/post.php?p=<?= $i?>">
+        <h4>red more</h4>
+         </a>
         </div>
-      </a>
+        
+     
+      
        </div>
    <?php }?>
 </article>
